bebc7b24e54489d54936d52e5646e8a5eb15ab28
kuixu <kuixu.cs@gmail.com>
2021-02-10 16:19:06 +0800

    first commit

--- tools/main.py
+++ tools/main.py
@@ -301,7 +301,7 @@ def main():
     print("Write prediction: epoch:{}, loss:{}".format(min_epoch, min_loss))
     
     # saving model
-    torch.save(best_model.state_dict(), args.filename_prediction.replace('.txt', '.pt'))
+    torch.save(best_model.state_dict(), 'model.pt'))
         
 if __name__ == '__main__':
     main()
